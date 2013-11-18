bda036eef5b99b35b3dab4a341506641007597df
Pruno <luca.bruno.it@gmail.com>
2013-11-18 12:04:53 +0100

    Adding Strategies for mongo driver default types

---
+++ src/MongoDbVirtualCollections/Hydrator/Strategy/MongoMaxKeyStrategy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MongoDbVirtualCollections\Hydrator\Strategy;
+
+use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
+
+/**
+ * Class MongoMaxKeyStrategy
+ * @package MongoDbVirtualCollections\Hydrator\Strategy
+ */
+class MongoMaxKeyStrategy implements StrategyInterface
+{
+    /**
+     * In order to allow inserting null value instead of a \MongoMaxKey boolean FALSE must be passed
+     *
+     * @param mixed $value
+     * @return \MongoMaxKey
+     */
+    public function extract($value)
+    {
+        return $value !== false ? new \MongoMaxKey() : null;
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    public function hydrate($value)
+    {
+        return $value;
+    }
+}
