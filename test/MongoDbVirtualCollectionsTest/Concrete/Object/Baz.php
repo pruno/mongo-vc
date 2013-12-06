1ba7df90a53ec71ce5683d40afe7a2445c3afca0
Pruno <luca.bruno.it@gmail.com>
2013-12-06 23:17:39 +0100

    Fixes #4

---
+++ test/MongoDbVirtualCollectionsTest/Concrete/Object/Baz.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MongoDbVirtualCollectionsTest\Concrete\Object;
+
+use MongoDbVirtualCollections\Model\AbstractObject;
+
+/**
+ * Class Baz
+ * @package MongoDbVirtualCollectionsTest\Concrete\Object
+ */
+class Baz extends AbstractObject
+{
+    public $baz1;
+
+    public $baz2;
+}
---
+++ test/MongoDbVirtualCollectionsTest/Concrete/VirtualCollection/BazCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MongoDbVirtualCollectionsTest\Concrete\VirtualCollection;
+
+use MongoDbVirtualCollections\Model\AbstractVirtualCollection;
+use MongoDbVirtualCollectionsTest\Concrete\Object\Baz;
+
+/**
+ * Class BazCollection
+ * @package MongoDbVirtualCollectionsTest\Concrete\VirtualCollection
+ */
+class BazCollection extends AbstractVirtualCollection
+{
+    /**
+     * @var string
+     */
+    protected $alias = 'Bazs';
+
+    /**
+     * @return Baz
+     */
+    public function createObjectPrototype()
+    {
+        return new Baz($this);
+    }
+}
